01808854bba6ae34cb968d17e393966b85ea6805
Edward Lim <edward.lim@redcypress.com.tw>
2018-08-10 14:59:30 +0800

    add mysql

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,7 @@
  * Created by wangyipeng on 2017/9/27.
  */
 import services from '../../helper/services'
+import { query }  from "../async-db"
 const router = require('koa-router')()
 router.get('/index', async function (ctx, next) {
   let advList = await services.local.getUserList()
@@ -9,6 +10,20 @@ router.get('/index', async function (ctx, next) {
 })
 
 router.get('/list', async function (ctx, next) {
+ 
+/*
+// access database demo
+async function selectAllData( ) {
+  let sql = 'SELECT * FROM db'
+  let dataList = await query( sql )
+  return dataList
+}
+
+async function getData() {
+  let dataList = await selectAllData()
+}
+
+getData();*/
   ctx.body = [
     {
       name: 'yipeng', age: '29'
