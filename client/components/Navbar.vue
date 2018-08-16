b8864a79267bce69d1c351d91ed8725c63e5cf11
DFTECH\PaulKHH <paul.ku@redcypress.com.tw>
2018-08-16 15:00:01 +0800

    about us base html wireframe and related css.

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -3,11 +3,13 @@
   <!-- header -->
   <uiv-navbar fixed-top>
     <a class="navbar-brand" slot="brand" href="#">Logo</a>
-    <uiv-navbar-text>About Us</uiv-navbar-text>
-    <uiv-navbar-text>Product</uiv-navbar-text>
-    <uiv-navbar-text>What's News</uiv-navbar-text>
-    <uiv-navbar-text>Contact</uiv-navbar-text>
-    <uiv-navbar-text>Demo</uiv-navbar-text>
+    <template slot="collapse">
+      <uiv-navbar-text>About Us</uiv-navbar-text>
+      <uiv-navbar-text>Product</uiv-navbar-text>
+      <uiv-navbar-text>What's News</uiv-navbar-text>
+      <uiv-navbar-text>Contact</uiv-navbar-text>
+      <uiv-navbar-text right>Demo</uiv-navbar-text>
+    </template>
   </uiv-navbar>
 
 </template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,69 +1,24 @@
 <template>
   <div class="container-fluid">
 
-    <banner/>
+    <banner />
 
     <!-- Ideas -->
-    <div class="row">
-      <div class="col-xs-6">
-        <!-- hexgen -->
-        <div class="hexgen">
-          <ol class="even">
-            <li class='hex'></li>
-            <li class='hex'></li>
-          </ol>  
-          <ol class="odd">
-            <li class='hex'></li>
-            <li class='hex invisible'></li>
-            <li class='hex'></li>
-          </ol>
-          <ol class="even">
-            <li class='hex'></li>
-            <li class='hex'></li>
-          </ol>
-        </div>  
-        
-        <!-- decorate curve line -->
-        <div class="curve">
-          <svg width="360" height="720" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0 Q400 360,0 720" stroke="#8c8c8c" stroke-width="2" fill="transparent"/>
-            <!-- one -->
-            <circle cx="30" cy="30" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="65" y="35">Icon Title One</text>
-
-            <!-- two -->
-            <circle cx="90" cy="95" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="120" y="100">Icon Title Two</text>
-
-            <!-- three -->
-            <circle cx="140" cy="160" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="170" y="165">Icon Title Three</text>
-
-            <!-- four -->
-            <circle cx="140" cy="570" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="170" y="575">Icon Title Four</text>
-
-            <!-- five -->
-            <circle cx="90" cy="630" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="120" y="635">Icon Title Five</text>
-
-            <!-- six -->
-            <circle cx="30" cy="690" r="10" stroke="#8c8c8c" stroke-width="1" fill="white" />
-            <text x="60" y="695">Icon Title Six</text>
-
-          </svg>
+    <div class="about-us">
+      
+      <!-- hexgen -->
+      <div class="hexagon">
+        <span></span>
+        <div>
+          <!-- features -->
+          <div class="features"></div>
+          <div class="features"></div>
+          <div class="features"></div>
+          <div class="features"></div>
+          <div class="features"></div>
+          <div class="features"></div>
         </div>
-        
-      </div>
-
-      
-
-      <!-- idea content -->
-      <div class="col-xs-6 benefits">
-        <h3>Highlight key benefits of MG Plus for our customers</h3>
-      </div>
-      
-
+      </div>     
       
     </div>
 
@@ -112,10 +67,9 @@
     },
     data () {
       return {
-        collapse: false,
         interval: 5000,
         indicators: true,
-        controls: true,
+        controls: false,
         slides: [
           {title: 'Slide 1'},
           {title: 'Slide 2'},
@@ -143,70 +97,93 @@
 
 <style scoped lang="scss">
 
-  /* hexgen css block */
-  $hex-size: 6em; 
-  .hexgen {
-    margin-top: 120px;
-    margin-bottom: 100px;
-    margin-left: 60px;
-    line-height: 1.3;
+  .about-us {
+    width: 100vw;
+    height: 60vh
   }
-  ol.even {
-    position: relative;
-    left: ($hex-size / 1.1);
-  }
-  ol.odd {
-    position: relative;
-    margin-top: -3.5%;
-    margin-bottom: -3.5%;
-  }
-  .hex {
-    z-index: 100;
-    position: relative;
-    margin: 1em auto;
-    width: $hex-size;
-    height: ($hex-size * 1.7);
-    background: #ccc;
-    transform: rotate(-90deg);
-    display: inline-block;
-    margin-right: ($hex-size / 1.10);
-    transition: all 150ms ease-in-out;
 
-    /* to hide middle hex  */
-    &.invisible {
-      opacity: 0;
-      cursor: initial;
-    }
+  @media only screen and (min-width: 768px) {
+    .hexagon {
+      position: relative;
+      width: 300px; 
+      height: 173.21px;
+      background-color: transparent;
+      margin: calc(30vh + 86.60px) auto;
 
-    &:before, &:after {
-      position: absolute;
-      width: inherit;
-      height: inherit;
-      border-radius: inherit;
-      background: inherit;
-      content: '';
+      &:before,
+      &:after {
+        content: "";
+        position: absolute;
+        width: 0;
+        border-left: 150px solid transparent;
+        border-right: 150px solid transparent;
+      }
+
+      &:before {
+        bottom: 100%;
+        border-bottom: 86.60px solid transparent;
+      }
+
+      &:after {
+        top: 100%;
+        width: 0;
+        border-top: 86.60px solid transparent;
+      }
     }
 
-    &:before {
-      transform: rotate(60deg);
-    }
-    &:after {
-      transform: rotate(-60deg);
-    }
+    
+    .features {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+        border-width: 1px;
+        border-style: solid;
+        border-color: black;
+        position: absolute;
+        z-index: 3;
 
-    &:hover {
-      transform: scale(1.5) rotate(-90deg);
-    }
+        &:first-child {
+          margin-left: 90px;
+          margin-top: -206.6px;
+        }
+
+        &:nth-child(2) {
+          margin-left: 300px;
+          margin-top: -86.6px;
+        }
+
+        &:nth-child(3) {
+          margin-left: 300px;
+          margin-top: 123.21px;
+        }
+
+        &:nth-child(4) {
+          margin-left: 90px;
+          margin-top: 259.81px;
+        }
+
+        &:nth-child(5) {
+          margin-left: -120px;
+          margin-top: -86.6px;
+        }
+
+        &:nth-child(6) {
+          margin-left: -120px;
+          margin-top: 123.21px;
+        }
+        
+      }
   }
-  .curve {
-    height: 100%;
-    position: absolute;
-    top: 0px;
-    left: 400px;
-  }  
-  .benefits {
-    margin-top: 320px;
-  } 
-  /* hexgen css block */
+  @media only screen and (min-width: 768px) {
+
+  }
+
   
+
+  
+
+  
+
+  
+
 </style>
