20684ec3b39a2b47e0dd1a7cbb20b31333a0de9b
DFTECH\PaulKHH <paul.ku@redcypress.com.tw>
2018-08-17 11:23:14 +0800

    MGPLUS-30 product section animation rwd bug fix and code refactor

--- client/components/Products.vue
+++ client/components/Products.vue
@@ -4,28 +4,28 @@
 
       <div class="product-intro">
         <div class="product-brand">
-          <img class="brand-img" src="~/assets/img/logo.png"/>
+          <img class="brand-img" src="~/assets/img/products/mgfun.png"/>
         </div>
         <p class="intro">Discover fun & interactive non-slot games thea will get platers hooked in no time!</p>
       </div>
 
       <div class="product-intro">
         <div class="product-brand">
-          <img class="brand-img" src="~/assets/img/logo.png"/>
+          <img class="brand-img" src="~/assets/img/products/mgfun.png"/>
         </div>
         <p class="intro">A revolutionary live dealer product engineered for the best mobile experience. Success? It's already in your hands.</p>
       </div>
 
       <div class="product-intro">
         <div class="product-brand">
-          <img class="brand-img" src="~/assets/img/logo.png"/>
+          <img class="brand-img" src="~/assets/img/products/mglive.png"/>
         </div>
         <p class="intro">The largest and most diverse portfolio of gmes in the industry and the home of the biggest and best games in Asia. Step into our world now!</p>
       </div>
+
     </section>
   </div>
-  
-  
+
 </template>
 
 <script lang="ts">
@@ -35,59 +35,46 @@
   @Component
   export default class product extends Vue {
 
+    resizeEvent() {
+      const intros = Array.from(document.querySelectorAll('.product-intro'));
+      const introBaseWidth: number = window.innerWidth / intros.length;
+
+      intros.forEach(intro => {
+        const introEl = intro as HTMLElement;
+        if (window.innerWidth <= 768) { 
+          introEl.style.width  = window.innerWidth + "px";
+          introEl.style.height = window.innerHeight/2 + "px";
+        } else {
+          introEl.style.width  = introBaseWidth + "px";
+          introEl.style.height = introBaseWidth + "px";
+        }
+      });
+    }
+
     mounted () {
       const intros = Array.from(document.querySelectorAll('.product-intro'));
       const introBaseWidth: number = window.innerWidth / intros.length;
 
-      for (let intro of intros) {
-        for (let intro of intros) {
-          const introEl = intro as HTMLElement;
-          if (window.innerWidth <= 768) { 
-              introEl.style.width  = window.innerWidth + "px";
-              introEl.style.height = window.innerHeight/2 + "px";
-            
-          } else {
-            introEl.style.width  = introBaseWidth + "px";
-            introEl.style.height = introBaseWidth + "px";
-          }
-        }
-        // intro base width
-        // (intro as HTMLElement).style.width  = introBaseWidth + "px";
-        // (intro as HTMLElement).style.height = introBaseWidth + "px";
+      this.resizeEvent();
 
+      intros.forEach(intro => {
         // mouse event
         intro.addEventListener('mouseover', (ev) => {
           if (window.innerWidth > 768) {
             TweenLite.to(intro, 0.3, {css: {width: introBaseWidth + 250 + "px"}});
           }
-          
         })
+        
         intro.addEventListener('mouseout', (ev) => {
           if (window.innerWidth > 768) {
             TweenLite.to(intro, 0.3, {css: {width: introBaseWidth + "px"}});
-          }
-          
+          }          
         })
-      }
+      });
 
-      // const section = document.querySelector('body') as HTMLElement;
-      window.addEventListener('resize', function() {
-          console.log('section resize.');
-          // for (let intro of intros) {
-          //   const introEl = intro as HTMLElement;
-          //   if (window.innerWidth <= 768) { 
-          //       introEl.style.width  = window.innerWidth + "px";
-          //       introEl.style.height = window.innerHeight + "px";
-              
-          //   } else {
-          //     introEl.style.width  = introBaseWidth + "px";
-          //     introEl.style.height = introBaseWidth + "px";
-          //   }
-          // }
-      })
+      window.addEventListener('resize', this.resizeEvent)
     }
 
-    
   }
 </script>
 
@@ -139,11 +126,10 @@
         padding: 0;
         height: 100%;
         text-align: center;
-        vertical-align: middle;
 
         .product-brand{
           padding-top: 60px;
-          width: 60%;
+          width: 54%;
           margin: 0 auto;
           img {
             margin-bottom: 40px;
